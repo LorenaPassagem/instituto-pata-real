2efa3c828974bf2e2d4708f4f4720639796b1af8
Lorena Cunha <lorenapassagem@gmail.com>
2020-08-11 16:09:40 -0300

    Updates

---
+++ precache-manifest.c5617c22a61dd8011e41bce0d2cc7ec1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee50255a8ea87b4a5d080264656b619c",
+    "url": "/instituto-pata-real/index.html"
+  },
+  {
+    "revision": "6c0d94c8de7203aa74a7",
+    "url": "/instituto-pata-real/static/css/main.deda295a.chunk.css"
+  },
+  {
+    "revision": "20388eb05658ecb82440",
+    "url": "/instituto-pata-real/static/js/2.ce1cd3ad.chunk.js"
+  },
+  {
+    "revision": "6c0d94c8de7203aa74a7",
+    "url": "/instituto-pata-real/static/js/main.02033d73.chunk.js"
+  },
+  {
+    "revision": "abbe49479ff8aa76b4a3",
+    "url": "/instituto-pata-real/static/js/runtime-main.22b7dfac.js"
+  },
+  {
+    "revision": "8ed5ffc4fc21c4539e3138d778450e0d",
+    "url": "/instituto-pata-real/static/media/banner-cachorro.8ed5ffc4.png"
+  },
+  {
+    "revision": "eb7e4497557d560b7e47de79322eb5fb",
+    "url": "/instituto-pata-real/static/media/filhote.eb7e4497.png"
+  },
+  {
+    "revision": "2039b75dab2daf8bfd22c66e6f8f6a40",
+    "url": "/instituto-pata-real/static/media/gatinho.2039b75d.png"
+  }
+]);
